da3074ebe84b2c9ba79cd304cff09c4615c14568
Ivo Stichel <77754587+istichel@users.noreply.github.com>
2025-11-30 12:09:09 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,7 @@
 apt update;
 apt install -y git;
 apt -y upgrade;
-locale-gen de_DE.UTF-8
-locale-gen de_DE
-update-locale LANG=de_DE.UTF-8
-dpkg-reconfigure locales
+#locale-gen de_DE.UTF-8
+#locale-gen de_DE
+#update-locale LANG=de_DE.UTF-8
+#dpkg-reconfigure locales
